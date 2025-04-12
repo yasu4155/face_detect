d855649ac46d42b49008767467fd1b3589ee37e6
yasu <yasu4155@gmail.com>
2025-04-12 15:29:30 +0900

    5th commit

--- face_detect.py
+++ face_detect.py
@@ -29,31 +29,17 @@ def main():
 #   STEP2：イメージオブジェクトの作成
     image = mp.Image.create_from_file(TARGET_IMG)
 
-#   STEP 4: 顔の検出
+#   STEP 3: 顔の検出
     detection_result = detector.detect(image)
 
-#   STEP 5: 検出した部位がわかるようにマーカーを描く
+#   STEP 4: 検出した部位がわかるようにマーカーを描く
     annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
     st.image(annotated_image)
 
-    img_height, img_width, _= img.shape
-    landmark_pixel_coordinates = []
+#   STEP 5: 検出した顔に点でマスクを描く
+    img2 = draw_mask_on_image(img, detection_result, TARGET_IMG)
+    st.image(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
 
-    for idx, face_landmark in enumerate(detection_result.face_landmarks):
-        for idx, landmark in enumerate(face_landmark):
-            x_pixcel = min(math.floor(landmark.x * img_width),  img_width-1)
-            y_pixcel = min(math.floor(landmark.y * img_height), img_height-1)
-            nornmalized_landmark = (x_pixcel, y_pixcel)
-            landmark_pixel_coordinates.append(nornmalized_landmark)
-    # print(landmark_pixel_coordinates)
-
-    coordinates_tuple = np.array(tuple(landmark_pixel_coordinates))
-    img = cv2.imread(TARGET_IMG)
-
-    for idx, coordinates in enumerate(coordinates_tuple):
-        cv2.circle(img, coordinates, 1, (255, 255, 0), 1)
-
-    st.image(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     os.remove('./target.jpg')
     return
 
@@ -68,19 +54,34 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     # Draw the face landmarks.
       face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
       face_landmarks_proto.landmark.extend([
-        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks
-      ])
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks])
 
       solutions.drawing_utils.draw_landmarks(
-          image=annotated_image,
-          landmark_list=face_landmarks_proto,
-          connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-          landmark_drawing_spec=None,
-          connection_drawing_spec=mp.solutions.drawing_styles
-          .get_default_face_mesh_contours_style())
-
+          image = annotated_image,
+          landmark_list = face_landmarks_proto,
+          connections = mp.solutions.face_mesh.FACEMESH_CONTOURS,
+          landmark_drawing_spec = None,
+          connection_drawing_spec = mp.solutions.drawing_styles.get_default_face_mesh_contours_style())
     return annotated_image
 
+def draw_mask_on_image(img, detection_result, TARGET_IMG):
+    img_height, img_width, _= img.shape
+    landmark_pixel_coordinates = []
+
+    for idx, face_landmark in enumerate(detection_result.face_landmarks):
+        for idx, landmark in enumerate(face_landmark):
+            x_pixcel = min(math.floor(landmark.x * img_width),  img_width-1)
+            y_pixcel = min(math.floor(landmark.y * img_height), img_height-1)
+            nornmalized_landmark = (x_pixcel, y_pixcel)
+            landmark_pixel_coordinates.append(nornmalized_landmark)
+    # print(landmark_pixel_coordinates)
+
+    coordinates_tuple = np.array(tuple(landmark_pixel_coordinates))
+    img2 = cv2.imread(TARGET_IMG)
+
+    for idx, coordinates in enumerate(coordinates_tuple):
+        cv2.circle(img2, coordinates, 1, (255, 255, 0), 1)
+    return img2
 
 if __name__ == '__main__':
     main()
